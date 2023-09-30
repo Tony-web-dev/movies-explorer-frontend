7c6251c861243d24df54f648597f3cd8489f640c
Antonina Timush <antoninatimush@yandex.ru>
2023-09-30 21:55:35 +0300

    empty

--- src/components/Main/SectionTitle/SectionTitle.jsx
+++
@@ -1,7 +0,0 @@
-import "./SectionTitle.css";
-
-export default function SectionTitle({ variableTitleClass, children }) {
-  return (
-    <h2 className={`section-title ${variableTitleClass}`}>{children}</h2>
-  )
-};
